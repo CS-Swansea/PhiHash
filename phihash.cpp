d0379e7d88427252b48344c2ff2c138cd497adca
Joss Whittle <joss.whittle@l2program.co.uk>
2014-02-27 22:43:12 +0000

    make: simple min hash

--- phihash.cpp
+++ phihash.cpp
@@ -3,38 +3,77 @@
 
 int main() {
 
-	char *hw = "Hello World!!!";
+	srand(time(NULL));
+
+	char *minStr = new char[HASH_LEN + 1];
+	unsigned char *minHash = new unsigned char[HASH_LEN + 1];
+	char * minHashStr = new char[HASH_STR];
+	memset(minHashStr, 0, HASH_STR);
+
+	char *str = new char[HASH_LEN + 1];
+	unsigned char *ustr = (unsigned char *) str;
+	str[HASH_LEN] = '\0';
+	
+	for (int i = 0; i < HASH_LEN; i++) {
+		str[i] = 32 + ((unsigned char) (rand() % 95));
+	}
+
 	unsigned char * hash = new unsigned char[HASH_LEN];
-	char * hashStr = new char[HASH_STR];
-
 	memset(hash, 0, HASH_LEN);
-	memset(hashStr, 0, HASH_STR);
 
-	genHash(hw, hash);
-	hash2Str(hash, hashStr);
+	memcpy(minStr, str, HASH_LEN+1);
+	genHash(minStr, minHash);
 
-	std::cout << hw << std::endl;
-	std::cout << hashStr << std::endl;
+	hash2Str(minHash, minHashStr);
+	std::cout << minStr << std::endl << std::endl;
+	std::cout << minHashStr << std::endl;
+
+	for (int i = 0; i < ITT; i++) {
+		
+		int c = 63;
+		while (c >= 0 && ustr[c] >= 132) c--;
+		
+		if (c < 0) {
+			for (int i = 0; i < HASH_LEN; i++) {
+				str[i] = 32 + ((unsigned char) (rand() % 95));
+			}
+		}
+		else {
+			ustr[c]++;
+		}
+
+		genHash(str, hash);
+
+		if (cmpHash(hash, minHash)) {
+			memcpy(minHash, hash, HASH_LEN);
+			memcpy(minStr, str, HASH_LEN);
+		}
+	}
+
+	hash2Str(minHash, minHashStr);
+	std::cout << minStr << std::endl << std::endl;
+	std::cout << minHashStr << std::endl;
 
 	delete [] hash;
-	delete [] hashStr;
-	
+	delete [] minHashStr;
+
+	PAUSE
 	return 0;
 };
 
 inline void genHash(char *a, unsigned char *hash) {
-	sha512((const unsigned char *) a, strlen(a), (unsigned char *) hash, 0);
+	sha512((unsigned char *)a, HASH_LEN, hash, 0);
 };
 
 inline bool cmpHash(unsigned char *a, unsigned char *b) {
 	for (int i = 0; i < HASH_LEN; i++) {
-		if (a[i] > b[i]) return false;
+		if ((int) a[i] < (int) b[i]) return true;
 	}
-	return true;
+	return false;
 };
 
 inline void hash2Str(unsigned char *hash, char *a) {
 	for (int i = 0; i < HASH_LEN; i++) {
-		sprintf(&(a[i * 2]), "%02X", (unsigned char)hash[i]);
+		sprintf(&(a[i * 2]), "%02X", hash[i]);
 	}
 };
--- phihash.hpp
+++ phihash.hpp
@@ -1,7 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <ctime>
+#include <omp.h>
 #include "sha512.hpp"
+
+#if defined(_MSC_VER)
+#define PAUSE system("PAUSE");
+#else
+#define PAUSE 
+#endif
+
+#define ITT 10000
 
 #define HASH_LEN 64
 #define HASH_STR ((HASH_LEN * 2) + 1)
