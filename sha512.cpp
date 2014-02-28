b1a3e3dea4a583a02333263e6f52c601ce8d490b
Joss Whittle <joss.whittle@l2program.co.uk>
2014-02-28 12:19:12 +0000

    PolarSSL license details added. Repo also brought under GPLv2

--- sha512.cpp
+++ sha512.cpp
@@ -1,4 +1,62 @@
-#include "sha512.hpp"
+/**
+ * \file sha512.cpp
+ *
+ * \brief SHA-384 and SHA-512 cryptographic hash function
+ *
+ *  Original Copyright (C) 2006-2013, Brainspark B.V.
+ *
+ *  This file is modified from a part of PolarSSL (http://www.polarssl.org)
+ *  Lead Maintainer: Joss Whittle <637342 @ swansea.ac.uk>
+ *
+ *	CHANGES FROM ORIGINAL: This file and it's accompanying .hpp have been modified 
+ *  to remove any external dependencies. They are now a stand-alone implementation
+ *  of the SHA-512 & SHA-384 hash functions. They can be included and compiled on
+ *  at least the following C++ compilers: G++, MSVC, and ICPC (the intel compiler)  
+ *
+ *  All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/** Original Licence Header
+ * \file sha512.cpp
+ *
+ * \brief SHA-384 and SHA-512 cryptographic hash function
+ *
+ *  Copyright (C) 2006-2013, Brainspark B.V.
+ *
+ *  This file is part of PolarSSL (http://www.polarssl.org)
+ *  Lead Maintainer: Paul Bakker <polarssl_maintainer at polarssl.org>
+ *
+ *  All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ #include "sha512.hpp"
 #include <cstdio>
 
 /*
