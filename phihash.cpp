166ee810a44a5d58272c0f8a74bfd62978f5610f
Joss Whittle <joss.whittle@l2program.co.uk>
2014-03-01 00:42:45 +0000

    make: 1 core idle

--- phihash.cpp
+++ phihash.cpp
@@ -37,7 +37,7 @@ int main() {
 		{
 			newHash = false;
 
-			#pragma omp parallel private(RNG_STATE)
+			#pragma omp parallel num_threads(__THREADS__) private(RNG_STATE)
 			{
 				// Setup RNG
 				RNG_STATE = seedThreadSafeRNG(omp_get_thread_num());
--- port.hpp
+++ port.hpp
@@ -5,10 +5,12 @@
 * closing the console when the program terminates...
 */
 #if defined(_MSC_VER)
+#define __THREADS__ 7
 #define PAUSE system("PAUSE");
 #define OFFLOAD_DECL 
 #else
 #include <offload.h>
+#define __THREADS__ 31
 #define PAUSE 
 #define OFFLOAD_DECL //__declspec(target(mic))
 #endif
