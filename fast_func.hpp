e9c76a3d4aaf546986149142133f0366af8a5ebc
Joss Whittle <joss.whittle@l2program.co.uk>
2014-03-03 19:12:06 +0000

    defined fast avx 64 byte copy function

---
+++ fast_func.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <immintrin.h>
+
+#define I32B __m256i
+#define STORE_I32B(ptrD, ptrS) _mm256_store_si256(ptrD, *ptrS)
+
+/**
+ * Copies the content of one 32 byte aligned 
+ * 64 byte buffer into another
+ *
+ * @param dest
+ *		A pointer to the destination buffer
+ *
+ * @param src
+ *		A pointer to the source buffer
+ */
+#define A_memcpy_64(dest,src)	{															\
+									I32B *A_memcpy_64_bufferS = (I32B*) (src);				\
+									I32B *A_memcpy_64_bufferD = (I32B*) (dest);				\
+									STORE_I32B(A_memcpy_64_bufferD, A_memcpy_64_bufferS);   \
+									A_memcpy_64_bufferS++;									\
+									A_memcpy_64_bufferD++;									\
+									STORE_I32B(A_memcpy_64_bufferD, A_memcpy_64_bufferS);	\
+								}
